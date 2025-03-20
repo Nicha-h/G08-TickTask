d0396aeb67ff9a9c6699c12c61151af48b5a651b
unknown <nichahongsri@gmail.com>
2025-03-20 23:08:26 +0700

    create NavBar

---
+++ src/pages/Calendar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Calendar() {
+  return (
+    <div>
+      <h1>this is calendar</h1>
+    </div>
+  )
+}
+
+export default Calendar
---
+++ src/pages/Pomodoro.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pomodoro() {
+  return (
+    <div>
+      <h1>this is pomo</h1>
+    </div>
+  )
+}
+
+export default Pomodoro
