e0a4ef779b9880b08e5d3d676c63a924854a65ac
unknown <nichahongsri@gmail.com>
2025-04-07 16:40:58 +0700

    added database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-
+import pool from './database/db.js';
+import * as dotenv from 'dotenv';
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/tasks', async (c) => {
+  const [tasks] = await pool.query('SELECT * FROM tasks');
+  return c.json(tasks);
+});
+
+// Add a task
+app.post('/tasks', async (c) => {
+  const { title, user_id } = await c.req.json();
+  await pool.query('INSERT INTO tasks (title, user_id) VALUES (?, ?)', [title, user_id]);
+  return c.json({ success: true });
+});
 
 serve({
   fetch: app.fetch,
