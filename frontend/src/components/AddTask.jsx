7aa68af8c76c709e91cac75815b264b8edec0962
Nicha-h <nichahongsri@gmail.com>
2025-05-13 02:22:09 +0700

    Merge successfully

--- frontend/src/components/AddTask.jsx
+++ frontend/src/components/AddTask.jsx
@@ -1,29 +1,18 @@
-import {React, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 import addIcon from '@iconify-icons/mdi/add';
 import TaskModal from './modals/AddTaskModal';
 
 function AddTask() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const handleSave = (task) => {
-
-        onAddTask(task);
-        setIsModalOpen(false);
-    };
+  const navigate = useNavigate();
 
   return (
     <>
       <div 
         className='fixed bottom-[60px] right-[76px] w-[87px] h-[87px] rounded-full border-2 bg-primary flex items-center justify-center shadow-lg hover:scale-105 transition-transform cursor-pointer'
-        onClick={() => setIsModalOpen(true)}>
+        onClick={() => navigate("/add")}>
         <Icon icon={addIcon} className="w-10 h-10 text-black" />
       </div>
-      
-      <TaskModal 
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSave={handleSave}
-      />
     </>
   );
 }
