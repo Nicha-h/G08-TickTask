98cf19464a17a330cafd7c9e002d54eaf900583b
Nicha-h <nichahongsri@gmail.com>
2025-05-23 17:38:30 +0700

    the errors are bugging me

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useRef } from 'react';
 import Logo from '../assets/Logo.svg';
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
